80bea21014afbbbcfc22ea85c1e25fbbc7c80f36
Valeriia Bila <vsbelaya@gmail.com>
2024-08-28 17:51:57 +0300

    finished

---
+++ hw1/app.js
@@ -0,0 +1,45 @@
+"use strict"
+
+//1
+
+const number1 = 25;
+const number2 = 10;
+
+const result1 = number1 + number2;
+console.log(result1);
+
+const result2 = number1 - number2;
+console.log(result2);
+
+const result3 = number1 * number2;
+console.log(result3);
+
+const result4 = number1 / number2;
+console.log(result4);
+
+const result5 = number1 ** 2;
+console.log(result5);
+
+const result6 = Math.sqrt(number1);
+console.log(result6);
+
+
+// 2
+
+const text = '2024';
+const number = +text;
+console.log(typeof number);
+
+
+const year = 2024;
+const convertedToString = year.toString();
+console.log(typeof convertedToString);
+
+
+const user = 'Valeriia';
+!!user;
+console.log(!!user);
+
+const value = 0;
+!!value;
+console.log(!!value);
