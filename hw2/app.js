1ddff0ae9ae590a9ff8d51951ef067d5c0739c9a
Valeriia Bila <vsbelaya@gmail.com>
2024-08-30 20:06:13 +0300

    added hw2

---
+++ hw2/app.js
@@ -0,0 +1,17 @@
+"use strict"
+
+//1
+
+let numbers;
+for (numbers = 1; numbers <= 100; numbers++) {
+    console.log(numbers);
+}
+
+// console.log(numbers);
+
+
+// var points = new Array(100);
+//         var label = points.length;
+//         for (var i = 0; i < label; i++) {
+//             console.log(points[i]);
+//         }
